3bb3584d53df1542376e1118781f71568579fb1f
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2024-05-25 11:15:01 +0700

    Add concurrency & Implement Colly for crawlling

--- main.go
+++ main.go
@@ -1,19 +1,43 @@
 package main
 
 import (
-	fmt
+	"fmt"
+	"sync"
+
 	"github.com/gocolly/colly"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-	// crawl data using 
-	// id is number
-	// link https://pokedex.org/#/pokemon/:id 
-	// example https://pokedex.org/#/pokemon/1
-	// get the data from the website and store it in the database
-	// and save it in json file 
-
+	// Initialize a new collector
 	c := colly.NewCollector()
-	
+
+	// Use a wait group to handle multiple requests concurrently
+	var wg sync.WaitGroup
+
+	// Set up the callback for when the HTML element is found
+	c.OnHTML("h1.detail-panel-header", func(e *colly.HTMLElement) {
+		// Extract the Pokémon name from the <h1> tag
+		pokemonName := e.Text
+		fmt.Println("Pokemon Name:", pokemonName)
+	})
+
+	// Set up the callback for when a request is made
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	// Loop over the first 10 Pokémon IDs
+	for i := 1; i <= 10; i++ {
+		wg.Add(1) // Add to the wait group
+		go func(id int) {
+			defer wg.Done() // Mark this goroutine as done in the wait group
+			// Construct the URL
+			url := fmt.Sprintf("https://pokedex.org/#/pokemon/%d", id)
+			// Visit the URL
+			c.Visit(url)
+		}(i)
+	}
+
+	// Wait for all goroutines to finish
+	wg.Wait()
 }
