2af315cfb02cd41393d94d57cbdc636e6ff8e5e7
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2024-06-13 13:43:30 +0700

    Adding p2p sample

--- sample/client1.go
+++ sample/client1.go
@@ -1,105 +1,87 @@
-package sample
+package main
 
 import (
 	"fmt"
 	"net"
-	"sync"
+	"os"
+	"strings"
+	"syscall"
 	"time"
+
+	"golang.org/x/sys/unix"
 )
-const (
-	tcpPort = "8080"
-	udpPort = "8081"
-)
-type Game struct {
-	players []net.Conn
-	currentTurn int
-	mutex sync.Mutex
-}
-var result = make(chan string)
 
 func main() {
-	game := Game{}
-
-	go listenUdp()
-	go broadCasting()
-	
-	ln, err := net.Listen("tcp", ":" + tcpPort)
+	// Create the socket
+	fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_DGRAM, syscall.IPPROTO_UDP)
 	if err != nil {
-		panic(err)
-	}
-	defer ln.Close()
-
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			panic(err)
-		}
-		game.players = append(game.players, conn)
-		go game.handleConnection(conn)
+		fmt.Printf("Error creating socket: %s\n", err)
+		os.Exit(1)
 	}
 
-}
-
-func (game *Game) handleConnection(conn net.Conn) {
-	for {
-		buffer := make([]byte, 1024)
-		n, err := conn.Read(buffer)
-		if err != nil {
-			fmt.Println("Error reading: ", err)
-			return
-		}
-		message := string(buffer[:n])
-		fmt.Println("Received: ", message)
-		game.mutex.Lock()
-		if conn == game.players[game.currentTurn] {
-			game.currentTurn = (game.currentTurn + 1) % len(game.players)
-			game.players[game.currentTurn].Write([]byte(message))
-		}
-		game.mutex.Unlock()
+	// Enable SO_REUSEPORT to allow multiple applications to bind to the same port
+	if err := unix.SetsockoptInt(fd, unix.SOL_SOCKET, unix.SO_REUSEPORT, 1); err != nil {
+		fmt.Printf("Error setting SO_REUSEPORT: %s\n", err)
+		syscall.Close(fd)
+		os.Exit(1)
 	}
-}
 
+	// Enable SO_BROADCAST to allow broadcasting
+	if err := unix.SetsockoptInt(fd, unix.SOL_SOCKET, unix.SO_BROADCAST, 1); err != nil {
+		fmt.Printf("Error setting SO_BROADCAST: %s\n", err)
+		syscall.Close(fd)
+		os.Exit(1)
+	}
 
-func broadCasting(connectionInfo string) {
-	conn, err := net.DialUDP("udp", nil, &net.UDPAddr{
-		IP: net.IPv4bcast,
-		Port: 8081,
-	})
+	// Bind the socket
+	addr := &syscall.SockaddrInet4{Port: 3000}
+	copy(addr.Addr[:], net.IPv4zero.To4())
+	if err := syscall.Bind(fd, addr); err != nil {
+		fmt.Printf("Error binding socket: %s\n", err)
+		syscall.Close(fd)
+		os.Exit(1)
+	}
+
+	// Convert the file descriptor to a net.PacketConn to use with Go's net package
+	file := os.NewFile(uintptr(fd), "udp")
+	conn, err := net.FilePacketConn(file)
 	if err != nil {
-		panic(err)
+		fmt.Printf("Error converting file descriptor to net.PacketConn: %s\n", err)
+		syscall.Close(fd)
+		file.Close()
+		os.Exit(1)
 	}
 	defer conn.Close()
 
-	for {
-		_, err = conn.Write([]byte(connectionInfo))
-		if err != nil {
-			panic(err)
+	// Set up the broadcast address
+	bcastAddr := &net.UDPAddr{IP: net.IPv4bcast, Port: 3000}
+
+	// Start a goroutine to broadcast messages
+	go func() {
+		for {
+			message := "Hello fro broadcast!"
+			_, err := conn.WriteTo([]byte(message), bcastAddr)
+			if err != nil {
+				fmt.Printf("Error broadcasting: %s\n", err)
+				continue
+			}
+			fmt.Println("Broadcasted message:", message)
+			time.Sleep(1 * time.Second)
 		}
-		time.Sleep(2 * time.Second)
-	}
-}
+	}()
 
-func listenUdp() {
-	addr, err := net.ResolveUDPAddr("udp", ":8081")
-	if err != nil {
-		panic(err)
-	}
-
-	conn, err := net.ListenUDP("udp", addr)
-	if err != nil {
-		panic(err)
-	}
-	defer conn.Close()
+	// Read from the UDP connection
 	buffer := make([]byte, 1024)
-
+	fmt.Println("Listening on UDP port 3000")
 	for {
-		n, remoteAddr, err := conn.ReadFromUDP(buffer)
-		if err != nil {
-			fmt.Println("Error reading UDP message: ", err)
+		n, addr, err := conn.ReadFrom(buffer)
+		if strings.Contains(string(buffer[:n]), "Hello fro broadcast!") {
 			continue
 		}
-
-		message := string(buffer[:n])
-		fmt.Println("Received from ", remoteAddr, " message: ", message)
+		if err != nil {
+			fmt.Printf("Error reading from UDP: %s\n", err)
+			continue
+		}
+		fmt.Printf("Received '%s' from %s\n", string(buffer[:n]), addr.String())
 	}
 }
